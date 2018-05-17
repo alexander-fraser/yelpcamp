cb0b4a7d2c7e22a2fd8c9bc90d7d3d24797ae517
alexander <alexfraser4@gmail.com>
2018-05-16 20:33:30 -0400

    Add mongo database.

--- app.js
+++ app.js
@@ -1,43 +1,82 @@
-var campgrounds = [
-    { name: "Salmon Creek", image: "https://thumbor.forbes.com/thumbor/600x300/smart/https%3A%2F%2Fblogs-images.forbes.com%2Fdrewmarshall%2Ffiles%2F2015%2F07%2FCamping-along-a-ridge-at-Sequoia-National-Forest.jpg" },
-    { name: "Cedar Rapids", image: "http://chile.travel/wp-content/uploads/bfi_thumb/Camping-INACH-ACT250-mpo4pzd1ap2psy7lkn7j26qrkfg29r2og97iro1ha0.jpg" },
-    { name: "Norse Lake", image: "http://campadounia.com/wp-content/uploads/2014/05/Small-Beach-Camp-Adounia-Beach-Camping-Morocco-Essaouira-600x300.png" },
-    { name: "Salmon Creek", image: "https://thumbor.forbes.com/thumbor/600x300/smart/https%3A%2F%2Fblogs-images.forbes.com%2Fdrewmarshall%2Ffiles%2F2015%2F07%2FCamping-along-a-ridge-at-Sequoia-National-Forest.jpg" },
-    { name: "Cedar Rapids", image: "http://chile.travel/wp-content/uploads/bfi_thumb/Camping-INACH-ACT250-mpo4pzd1ap2psy7lkn7j26qrkfg29r2og97iro1ha0.jpg" },
-    { name: "Norse Lake", image: "http://campadounia.com/wp-content/uploads/2014/05/Small-Beach-Camp-Adounia-Beach-Camping-Morocco-Essaouira-600x300.png" },
-    { name: "Salmon Creek", image: "https://thumbor.forbes.com/thumbor/600x300/smart/https%3A%2F%2Fblogs-images.forbes.com%2Fdrewmarshall%2Ffiles%2F2015%2F07%2FCamping-along-a-ridge-at-Sequoia-National-Forest.jpg" },
-    { name: "Cedar Rapids", image: "http://chile.travel/wp-content/uploads/bfi_thumb/Camping-INACH-ACT250-mpo4pzd1ap2psy7lkn7j26qrkfg29r2og97iro1ha0.jpg" },
-    { name: "Norse Lake", image: "http://campadounia.com/wp-content/uploads/2014/05/Small-Beach-Camp-Adounia-Beach-Camping-Morocco-Essaouira-600x300.png" }
-]
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
 
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded( {extended: true} ));
 app.set("view engine", "ejs");
+
+// Mongo DB Schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+/* Campground.create(
+    { 
+        name: "Cedar Rapids", 
+        image: "http://chile.travel/wp-content/uploads/bfi_thumb/Camping-INACH-ACT250-mpo4pzd1ap2psy7lkn7j26qrkfg29r2og97iro1ha0.jpg",
+        description: "These are incredibly fast-flowing rapids. Dangerous. Stay Clear."
+    }, function(error, campground) {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log("New campground");
+            console.log(campground);
+        }
+    }
+); */
 
 app.get("/", function (req, res) {
     res.render("landing.ejs");
 });
 
+// INDEX
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds.ejs", {campgrounds: campgrounds});
+    Campground.find({}, function(error, allCampgrounds) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.render("index.ejs", { campgrounds: allCampgrounds });
+        }
+    });
 });
 
+// CREATE
 app.post("/campgrounds", function (req, res) {
     // Get data from form and add to campgrounds array.
-    var name = req.body.name
-    var image = req.body.image
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-
-    // Redirect back to campgrounds page (through the GET request).
-    res.redirect("/campgrounds");
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
+    
+    Campground.create(newCampground, function (error, newlyCreated) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });    
 });
 
+// NEW
 app.get("/campgrounds/new", function(req, res) {
     res.render("new.ejs");
+});
+
+// SHOW
+app.get("/campgrounds/:id", function (req, res) {
+    Campground.findById(req.params.id, function(error, foundCampground) {
+        if(error) {
+            console.log(error);
+        } else {
+            res.render("show.ejs", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(3000, function() {
